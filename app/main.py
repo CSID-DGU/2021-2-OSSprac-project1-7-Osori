f4adfbac45fb48525e0634cc210e0b62ba8ebb08
jihey330 <jiehy330@naver.com>
2021-11-27 01:09:48 +0900

    A

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+   return render_template('main.html')
+
+@app.route('/detail', methods = ['POST', 'GET'])
+def detail():
+   if request.method == 'POST':
+      detail = dict()
+      detail['Name'] = request.form.get('Name')
+      detail['StudentNumber'] = request.form.get('StudentNumber')
+      detail['Gender'] = request.form.get('Gender')
+      detail['Major'] = request.form.get('Major')
+      return render_template("detail.html",detail = detail)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, port=80)
