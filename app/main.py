504dbc3008d192c8fed67a91c9bb31e1092e2f7a
Thor <sangyun0904@gmail.com>
2021-11-28 02:23:06 +0900

    Modify detail.html and main.py dict name detail to data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+data = dict()
 
 @app.route('/')
 def student():
@@ -9,12 +10,11 @@ def student():
 @app.route('/detail', methods = ['POST', 'GET'])
 def detail():
    if request.method == 'POST':
-      detail = dict()
-      detail['Name'] = request.form.get('Name')
-      detail['StudentNumber'] = request.form.get('StudentNumber')
-      detail['Gender'] = request.form.get('Gender')
-      detail['Major'] = request.form.get('Major')
-      return render_template("detail.html",detail = detail)
+      data['Name'] = request.form.get('Name')
+      data['StudentNumber'] = request.form.get('StudentNumber')
+      data['Gender'] = request.form.get('Gender')
+      data['Major'] = request.form.get('Major')
+      return render_template("detail.html",data = data)
 
 if __name__ == '__main__': 
     app.run(host="0.0.0.0", debug=True, port=80)
