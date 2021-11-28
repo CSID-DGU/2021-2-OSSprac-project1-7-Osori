4d6fed70c6cf588064357a1b9de9a91d6459b1ba
ssosojeong <thanku1598@gmail.com>
2021-11-28 16:29:06 +0900

    C

--- app/main.py
+++ app/main.py
@@ -16,6 +16,16 @@ def detail():
       data['Major'] = request.form.get('Major')
       return render_template("detail.html",data = data)
 
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = dict()
+      result['Name'] = request.form.get('Name')
+      result['StudentNumber'] = request.form.get('StudentNumber')
+      result['Gender'] = request.form.get('Gender')
+      result['Major'] = request.form.get('Major')
+      return render_template("result.html",result = result)
+
 if __name__ == '__main__': 
     app.run(host="0.0.0.0", debug=True, port=80)
 
